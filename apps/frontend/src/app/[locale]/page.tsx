e68737189192345dfc74c5b76abcdaca3743772b
Sergio Acosta <sergioacostaquintana@gmail.com>
2025-08-21 16:50:09 +0100

    fix(page): 🛠️ Update revalidation time to 15 minutes
    * Increased the revalidation time from 60 seconds to 15 minutes for better performance.

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { ProviderCTA } from "@/components/ui/ProviderCTA";
 import { TestimonialsGrid } from "@/components/ui/TestimonialsGrid";
 import TrendingExperiences from "@/features/TrendingExperiences";
 
-export const revalidate = 60;
+export const revalidate = 60 * 15; // 15 minutes
 
 export default function Home() {
   return (
