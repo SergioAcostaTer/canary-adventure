93b278fe0017cf94a1a2f717e118032025701c20
Sergio Acosta <sergioacostaquintana@gmail.com>
2025-08-23 00:45:07 +0100

    no static

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -1,19 +1,6 @@
 import AdventureHero from "@/features/home/Hero";
 import TrendingExperiences from "@/features/home/TrendingExperiences";
 
-export const revalidate = 3600; // 1 hour
-
-export const dynamic = "force-static";
-
-export function generateStaticParams() {
-  return [
-    { locale: "en" },
-    { locale: "es" },
-    { locale: "de" },
-    { locale: "fr" },
-  ];
-}
-
 export default function Home() {
   return (
     <div>
